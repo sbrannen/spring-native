7195f467ac548e276df3bb270b1b940ae40dcaaa
Sam Brannen <sam@sambrannen.com>
2021-11-03 15:35:42 +0100

    Introduce @Autowired field in CommandlinerunnerApplicationTests
    After adding the @Autowired field and running the tests within a native
    image, we see the following.
    
    JUnit Platform on Native Image - report
    ----------------------------------------
    
    com.example.commandlinerunner.CommandlinerunnerApplicationTests > contextLoads() FAILED
    
    Failures (1):
      JUnit Jupiter:CommandlinerunnerApplicationTests:contextLoads()
        MethodSource [className = 'com.example.commandlinerunner.CommandlinerunnerApplicationTests', methodName = 'contextLoads', methodParameterTypes = '']
        => org.opentest4j.AssertionFailedError: @Autowired CLR field ==> expected: not <null>
           org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:39)
           org.junit.jupiter.api.Assertions.fail(Assertions.java:134)
           org.junit.jupiter.api.AssertNotNull.failNull(AssertNotNull.java:47)
           org.junit.jupiter.api.AssertNotNull.assertNotNull(AssertNotNull.java:36)
           org.junit.jupiter.api.Assertions.assertNotNull(Assertions.java:308)
           com.example.commandlinerunner.CommandlinerunnerApplicationTests.contextLoads(CommandlinerunnerApplicationTests.java:17)
           java.lang.reflect.Method.invoke(Method.java:566)
           org.junit.platform.commons.util.ReflectionUtils.invokeMethod(ReflectionUtils.java:725)
           org.junit.jupiter.engine.execution.MethodInvocation.proceed(MethodInvocation.java:60)
           org.junit.jupiter.engine.execution.InvocationInterceptorChain$ValidatingInvocation.proceed(InvocationInterceptorChain.java:131)
           [...]
    
    Thus, dependency injection is not being performed on the test class
    instance.
    
    The SpringBootAotContextLoader and AotCacheAwareContextLoaderDelegate
    seem not to be used within the native image.

--- samples/commandlinerunner/src/test/java/com/example/commandlinerunner/CommandlinerunnerApplicationTests.java
+++ samples/commandlinerunner/src/test/java/com/example/commandlinerunner/CommandlinerunnerApplicationTests.java
@@ -1,14 +1,20 @@
 package com.example.commandlinerunner;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CommandlinerunnerApplicationTests {
 
+	@Autowired
+	CLR clr;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(this.clr, "@Autowired CLR field");
 	}
 
 }
